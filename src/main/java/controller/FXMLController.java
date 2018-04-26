cf8ba06ecc6adf8e35713abb5d6df05ee4e202d9
bvaldeiglesias <bruno.valdeiglesias@gmail.com>
2018-04-26 19:15:37 -0300

    Modificado presentacion

--- src/main/java/controller/FXMLController.java
+++ src/main/java/controller/FXMLController.java
@@ -1,12 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
@@ -17,62 +15,134 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import pruebasAleatoridad.ChiCuadrado;
+import nrosAleatorios.Congruencial;
 
 /**
  *
  * @author Bruno
  */
-public class FXMLController {
+public class FXMLController implements Initializable{
 
-    @FXML
-    private CheckBox chkCMixto;
-    @FXML
-    private TextField txtSemillaC;
     @FXML
     private Button btnProbar;
     @FXML
-    private TextField txtCantNrosC;
-    @FXML
-    private TextField txtCantIntervalosC;
-    @FXML
-    private Button btnReiniciarC;
-    @FXML
-    private TextArea txtAResultados;
-    @FXML
-    private BarChart<?, ?> chrtFrecuenciaC;
+    private BarChart chrtFrecuenciaC;
     @FXML
     private NumberAxis yFrecBc;
     @FXML
     private CategoryAxis xIntervalo;
     @FXML
-    private LineChart<?, ?> lnchrEsperadaC;
+    private LineChart lnchrEsperadaC;
     @FXML
     private NumberAxis yFrecLn;
     @FXML
-    private ListView<?> lsvSerie;
+    private RadioButton rdbUniforme;
     @FXML
-    private Label lbl_sumatoria_chi_cuadrado;
+    private ToggleGroup grpDistribucion;
     @FXML
-    private Label lbl_acepta_prueba;
+    private RadioButton rdbPoisson;
     @FXML
-    private TableView<?> tbl_Tabla;
+    private RadioButton rdbExponencial;
+    @FXML
+    private RadioButton rdbNormal;
+    @FXML
+    private Label lblAoMedia;
+    @FXML
+    private Label lblBoDesviacion;
+    @FXML
+    private TextField txtAoMedia;
+    @FXML
+    private TextField txtBoDesviacion;
+    @FXML
+    private TextField txtLambda;
+    @FXML
+    private ListView txfSerieGenerada;
+    @FXML
+    private TextField txtCantNros;
+    @FXML
+    private TextField txtCantIntervalos;
+    @FXML
+    private Button btnReiniciar;
+    @FXML
+    private Label lblSumatoriaChiCuadrado;
+    @FXML
+    private Label lblAceptaPrueba;
+    @FXML
+    private TableView tblTabla;
 
+    private double[] serie;
+    private ChiCuadrado pruebaChi;
+    private NumberAxis yFrecLn_B;
+    private Congruencial rndGenerator;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        tblTabla.setEditable(true);
 
-    @FXML
-    private void handleChkCMixto(ActionEvent event) {
+        TableColumn colIntervalo = new TableColumn("Intervalo");
+        TableColumn colFrec_obt = new TableColumn("Frecuencia Obtenida");
+        TableColumn colFrec_esp = new TableColumn("Frecuencia Esperada");
+        TableColumn col3 = new TableColumn("(fo-fe)^2");
+        TableColumn col4 = new TableColumn("(col 3)/fe");
+
+        colIntervalo.setCellValueFactory(new PropertyValueFactory<>("intervalo"));
+        colFrec_obt.setCellValueFactory(new PropertyValueFactory<>("frecuencia_obtenida"));
+        colFrec_esp.setCellValueFactory(new PropertyValueFactory<>("frecuencia_esperada"));
+        col3.setCellValueFactory(new PropertyValueFactory<>("col3"));
+        col4.setCellValueFactory(new PropertyValueFactory<>("col4"));
+
+        tblTabla.getColumns().addAll(colIntervalo, colFrec_obt, colFrec_esp, col3, col4);
+
+        tblTabla.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     }
-
+    
     @FXML
     private void handleButtonProbar(ActionEvent event) {
     }
+    
+    @FXML
+    private void handleButtonReiniciar(ActionEvent event) {
+    }
+
 
     @FXML
-    private void handleButtonReiniciarC(ActionEvent event) {
+    private void handleRdbUniforme(ActionEvent event) {
+        lblAoMedia.setText("A");
+        lblBoDesviacion.setText("B");
+        txtAoMedia.setDisable(false);
+        txtBoDesviacion.setDisable(false);
+        txtLambda.setDisable(true);
     }
-    
+
+    @FXML
+    private void handleRdbPoisson(ActionEvent event) {
+        txtAoMedia.setDisable(true);
+        txtBoDesviacion.setDisable(true);
+        txtLambda.setDisable(false);
+    }
+
+    @FXML
+    private void handleRdbExponencial(ActionEvent event) {
+        txtAoMedia.setDisable(true);
+        txtBoDesviacion.setDisable(true);
+        txtLambda.setDisable(false);
+    }
+
+    @FXML
+    private void handleRdbNormal(ActionEvent event) {
+        lblAoMedia.setText("Media");
+        lblBoDesviacion.setText("Desviacion");
+        txtAoMedia.setDisable(false);
+        txtBoDesviacion.setDisable(false);
+        txtLambda.setDisable(true);
+    }
+  
 }
