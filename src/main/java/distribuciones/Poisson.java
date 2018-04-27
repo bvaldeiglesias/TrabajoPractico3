a8999e5838a1fc37547c10aea428569f04b26312
juani <juani@192.168.0.56>
2018-04-27 10:00:38 -0300

    Clase Poisson arreglada (falta arreglar los intervalos de la grafica de poisson)

--- src/main/java/distribuciones/Poisson.java
+++ src/main/java/distribuciones/Poisson.java
@@ -1,6 +1,5 @@
 package distribuciones;
 
-
 public class Poisson
 {
 
@@ -19,47 +18,66 @@ public class Poisson
     }
 
     //Este método te devuelve el vector con todas las frecuencias esperadas de todos los intervalos
-    public double[] frecuencia_esperada_intervalo()
+//    public double[] frecuencia_esperada_intervalo()
+//    {
+//        temp = v;
+//        quickSort();
+//        double primer_valor = temp[0];
+//        double ultimo_valor = temp[temp.length - 1];
+//        double recorrido = ultimo_valor - primer_valor;
+//        double cant_x_int = recorrido / intervalo;
+//        double anterior = primer_valor;
+//        double posterior;
+//        for (int i = 0; i < intervalo; i++)
+//        {
+//            posterior = anterior + cant_x_int;
+//            double valor1 = probabilidad_ocurrencia_parcial(anterior);
+//            double valor2 = probabilidad_ocurrencia_parcial(posterior);
+//            double prob_ocurrencia = valor1 + valor2;
+//            double frec_esperada = prob_ocurrencia * v.length;
+//            frecuencia_esperada[i] = frec_esperada;
+//            anterior = posterior;
+//        }
+//        return frecuencia_esperada;
+//    }
+    
+        public double[] frecuencia_esperada_intervalo()
     {
+        frecuencia_esperada = new double[intervalo];
         temp = v;
         quickSort();
-        double primer_valor = temp[0];
-        double ultimo_valor = temp[temp.length - 1];
-        double recorrido = ultimo_valor - primer_valor;
-        double cant_x_int = recorrido / intervalo;
-        double anterior = primer_valor;
-        double posterior;
+        int primer_valor = (int) temp[0];
+        int ultimo_valor = (int) temp[temp.length - 1];
+        int recorrido = ultimo_valor - primer_valor;
+        int cant_x_int = (int) Math.floor(recorrido / intervalo);
+        int anterior = primer_valor - cant_x_int;
+        int posterior;
         for (int i = 0; i < intervalo; i++)
         {
             posterior = anterior + cant_x_int;
-            double valor1 = probabilidad_ocurrencia_parcial(anterior);
-            double valor2 = probabilidad_ocurrencia_parcial(posterior);
-            double prob_ocurrencia = valor1 + valor2;
-            double frec_esperada = prob_ocurrencia * v.length;
+            double prob_ocurrencia = probabilidad_ocurrencia_parcial(anterior) + probabilidad_ocurrencia_parcial(posterior);
+            double frec_esperada = Math.round(prob_ocurrencia * v.length);
             frecuencia_esperada[i] = frec_esperada;
-            anterior = posterior;
+            anterior = posterior + cant_x_int;
         }
         return frecuencia_esperada;
     }
 
-    public double probabilidad_ocurrencia_parcial(double x)
+    public double probabilidad_ocurrencia_parcial(int x)
     {
-        double numerador = Math.pow(lambda, x) * Math.exp(-lambda);
-        int valor_para_factorial = (int) Math.round(x);
-        double denominador = factorial(valor_para_factorial);
-        return (numerador/denominador);
+        double resultado = ((Math.pow(lambda, x)) * (Math.exp(-lambda))) / (factorial(x));
+        return resultado;
     }
 
-    public double factorial(int number)
+    public double factorial(int n)
     {
-        long result = 1;
-
-        for (int factor = 2; factor <= number; factor++)
+        if (n == 0)
         {
-            result *= factor;
+            return 1;
+        } else
+        {
+            return (n * factorial(n - 1));
         }
-
-        return result;
     }
 
     public void quickSort()
